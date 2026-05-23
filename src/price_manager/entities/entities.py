1e7ec4c36e2c9b0b5c098a82ba113391429c8e2a
AgostinaMerino <agosm.am@gmail.com>
2026-05-23 19:45:52 +0000

    Adaptacion de repositorios a base de datos

--- src/price_manager/entities/entities.py
+++ src/price_manager/entities/entities.py
@@ -12,6 +12,14 @@ class Categoria(EntidadBase):
     super().__init__(id)
     self.nombre = nombre
 
+  def __str__(self) -> str:
+    return (
+      f"Categoria("
+      f"id={self.id}, "
+      f"nombre='{self.nombre}'"
+      f")"
+    )
+
 
 class Proveedor(EntidadBase):
   def __init__(self, id: int, nombre: str, contacto: str):
@@ -19,18 +27,41 @@ class Proveedor(EntidadBase):
     self.nombre = nombre
     self.contacto = contacto
 
+  def __str__(self) -> str:
+    return (
+      f"Proveedor("
+      f"id={self.id}, "
+      f"nombre='{self.nombre}', "
+      f"contacto='{self.contacto}'"
+      f")"
+    )
+
 
 class Moneda(EntidadBase):
   def __init__(self, id: int, nombre: str):
     super().__init__(id)
     self.nombre = nombre
 
+  def __str__(self) -> str:
+    return (
+      f"Moneda("
+      f"id={self.id}, "
+      f"nombre='{self.nombre}'"
+      f")"
+    )
 
 class TipoCotizacion(EntidadBase):
   def __init__(self, id: int, nombre: str):
     super().__init__(id)
     self.nombre = nombre
 
+  def __str__(self) -> str:
+    return (
+      f"TipoCotizacion("
+      f"id={self.id}, "
+      f"nombre='{self.nombre}'"
+      f")"
+    )
 
 class Precio:
   def __init__(
@@ -46,6 +77,14 @@ class Precio:
     self.moneda = moneda
     self.fecha = fecha
 
+  def __str__(self) -> str:
+    return (
+      f"Precio("
+      f"valor={self.valor}, "
+      f"moneda='{self.moneda.nombre}', "
+      f"fecha='{self.fecha}'"
+      f")"
+    )
 
 class Producto(EntidadBase):
   def __init__(
@@ -64,6 +103,17 @@ class Producto(EntidadBase):
     self.categoria = categoria
     self.proveedor = proveedor
 
+  def __str__(self) -> str:
+    return (
+      f"Producto("
+      f"id={self.id}, "
+      f"nombre='{self.nombre}', "
+      f"precio={self.precio.valor} "
+      f"{self.precio.moneda.nombre}, "
+      f"categoria='{self.categoria.nombre}', "
+      f"proveedor='{self.proveedor.nombre}'"
+      f")"
+    )
 
 class Stock:
   def __init__(self, producto: Producto, cantidad: int):
@@ -73,6 +123,14 @@ class Stock:
     self.producto = producto
     self.cantidad = cantidad
 
+
+  def __str__(self) -> str:
+    return (
+      f"Stock("
+      f"producto='{self.producto.nombre}', "
+      f"cantidad={self.cantidad}"
+      f")"
+    )
 
 class CotizacionDolar:
   def __init__(
@@ -87,3 +145,12 @@ class CotizacionDolar:
     self.valor = valor
     self.fecha = fecha
     self.tipo = tipo
+
+  def __str__(self) -> str:
+    return (
+      f"CotizacionDolar("
+      f"tipo='{self.tipo.nombre}', "
+      f"valor={self.valor}, "
+      f"fecha='{self.fecha}'"
+      f")"
+    )
