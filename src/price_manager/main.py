704f3e27ecf1338120eed3be20449fb77409283e
AgostinaMerino <agosm.am@gmail.com>
2026-05-23 18:13:55 +0000

    Configuracion final del sistema

---
+++ src/price_manager/main.py
@@ -0,0 +1,27 @@
+
+from price_manager.models.models import crear_tablas
+from price_manager.migrations.migrations import migrar_datos
+from price_manager.ui.console import main as ejecutar_menu
+
+
+desactivar_git_push = False
+
+
+def inicializar_base_datos() -> None:
+  """Inicializa tablas y datos base."""
+
+  crear_tablas()
+  migrar_datos()
+
+
+def main(import_default_data: bool = False) -> None:
+  """Ejecuta el sistema."""
+
+  if import_default_data:
+    inicializar_base_datos()
+
+  ejecutar_menu()
+
+
+if __name__ == "__main__":
+  main(import_default_data=False)
